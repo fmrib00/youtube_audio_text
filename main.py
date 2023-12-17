fa01c3fe4042cadae8eafe7d38af69d9c4a16724
fmrib00 <yongyuezhang@gmail.com>
2023-12-17 18:31:47 +0800

    update

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
 import argparse
-import whisper
+# import whisper
 from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
 
 
-youtube_url = 'https://www.youtube.com/watch?v=NT2H9iyd-ms'
-audio_file = 'audio.mp4'
+def download_video(url: str, audio_only: bool) -> None:
+    try:
+        yt = YouTube(url)
+        if audio_only:
+            print('downloading audio: {}'.format(yt.title))
+            yt.streams.filter(only_audio=True).first().download()
+        else:
+            yt.streams.filter(progressive=True).get_highest_resolution().download()
+        print('download complete')
+    except VideoUnavailable:
+        print('video is unavailable')
+    except Exception as e:
+        print(e)
+        print('download failed')
 
-def download_video(url):
-    yt = YouTube(url)
-    print('downloading video: {}'.format(yt.title))
-    yt.streams.filter(only_audio=True).first().download(filename=audio_file)
-
-def transcribe_audio(output_file):
-    model = whisper.load_model('base')
-    print('whisperring....')
-    output = model.transcribe(audio_file)
-    with open(output_file, 'w') as f:
-        f.write(output['text'])
-    print('transcription complete, saved to {}'.format(output_file))
+# def transcribe_audio(output_file):
+#     model = whisper.load_model('base')
+#     print('whisperring....')
+#     output = model.transcribe(audio_file)
+#     with open(output_file, 'w') as f:
+#         f.write(output['text'])
+#     print('transcription complete, saved to {}'.format(output_file))
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--youtube_url', type=str, default=youtube_url)
-    parser.add_argument('--output_file', type=str, default='output.txt')
+    parser.add_argument('--url', type=str, required=True)
+    parser.add_argument('--audio_only', action='store_true')
     args = parser.parse_args()
 
-    download_video(args.youtube_url)
-    transcribe_audio(args.output_file)
+    download_video(args.url, args.audio_only)
+    # transcribe_audio(args.output_file)
 
 if __name__ == '__main__':
     main()
